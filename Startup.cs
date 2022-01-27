b12c8208a0230a4351232b11df05327781d7aaaa
khanhtoanioe <80748497+khanhtoanioe@users.noreply.github.com>
2022-01-27 14:09:12 +0700

    deploy api

--- Startup.cs
+++ Startup.cs
@@ -86,12 +86,12 @@ namespace ExamEdu
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+        //     if (env.IsDevelopment())
+        //     {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExamEdu v1"));
-            }
+            // }
 
             // ReFormat exception message
             app.UseExceptionHandler(e => e.Run(async context =>
