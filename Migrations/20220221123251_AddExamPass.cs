d2b943daf776f5939f5c7c304f065620bf2cd952
khanhtoanioe <80748497+khanhtoanioe@users.noreply.github.com>
2022-02-21 19:41:09 +0700

    Update submit Answer

---
+++ Migrations/20220221123251_AddExamPass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamEdu.Migrations
+{
+    public partial class AddExamPass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Exams",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Room",
+                table: "Exams",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "Room",
+                table: "Exams");
+        }
+    }
+}
