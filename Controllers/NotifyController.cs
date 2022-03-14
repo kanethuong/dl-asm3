651ede2820b33fb4cab5a5b566d509dd3100bac8
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2022-03-14 17:48:34 +0700

    add cache database to signalR

--- Controllers/NotifyController.cs
+++ Controllers/NotifyController.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BackEnd.Services.Cache;
+using examedu.Services.Classes;
 using ExamEdu.DTO;
 using ExamEdu.DTO.NotificationDTO;
+using ExamEdu.DTO.PaginationDTO;
+using ExamEdu.Helper;
 using ExamEdu.Hubs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
@@ -14,13 +18,22 @@ namespace examedu.Controllers
     [Route("api/[controller]")]
     public class NotifyController : ControllerBase
     {
+     
         private readonly IHubContext<NotifyHub> _notifyHub;
+        private readonly ICacheProvider _cacheProvider;
 
-        public NotifyController(IHubContext<NotifyHub> notifyHub)
+        private readonly IClassService _classService;
+      
+
+        public NotifyController(IHubContext<NotifyHub> notifyHub, ICacheProvider cacheProvider,
+            IClassService classService)
         {
+            _classService = classService;
             _notifyHub = notifyHub;
+            _cacheProvider = cacheProvider;
         }
 
+        
         /// <summary>
         /// invoke function ReceiveNotification and send data to trainee
         /// </summary>
@@ -35,9 +48,55 @@ namespace examedu.Controllers
             // }
             notifyMessage.SendTo = notifyMessage.SendTo.ToLower();
             notifyMessage.User = notifyMessage.User.ToLower();
-            //await _cacheProvider.AddValueToKey<NotifyMessage>(notifyMessage.SendTo, notifyMessage);
+            await _cacheProvider.AddValueToKey<NotifyMessage>(notifyMessage.SendTo, notifyMessage);
             await _notifyHub.Clients.Group(notifyMessage.SendTo.ToLower()).SendAsync("ReceiveNotification", notifyMessage);
             return Ok(new ResponseDTO(204, "Successfully invoke"));
+        }
+        /// <summary>
+        /// invoke function ReceiveHistory and send data 
+        /// </summary>
+        /// <param name="email">email of user want to recieveHistory</param>
+        /// <returns>404: email not found / 204: send success</returns>
+        [HttpGet("history")]
+        public async Task<ActionResult> SendHistory([FromQuery] string email, [FromQuery] PaginationParameter paginationParameter)
+        {
+            email = email.ToLower();
+            List<NotifyMessage> history = new List<NotifyMessage>();
+            history = await _cacheProvider.GetFromCache<List<NotifyMessage>>(email);
+            if (history == null)
+            {
+                return NotFound(new ResponseDTO(404, "history not found!"));
+            }
+
+            history.Sort((x, y) => y.CreatedAt.CompareTo(x.CreatedAt));
+
+            history = history.Where(t =>
+                t.User.Contains(paginationParameter.SearchName.ToLower()) ||
+                t.SendTo.Contains(paginationParameter.SearchName.ToLower())).Select(t => t).ToList();
+
+            return Ok(new PaginationResponse<IEnumerable<NotifyMessage>>(history.Count(), PaginationHelper.GetPage(history, paginationParameter)));
+        }
+        [HttpPost("setSeen")]
+        public async Task<ActionResult> SetSeen([FromBody] string email)
+        {
+            if (email == null)
+            {
+                return BadRequest(new ResponseDTO(404, "email not found"));
+            }
+            email = email.ToLower();
+            List<NotifyMessage> history = new List<NotifyMessage>();
+            history = await _cacheProvider.GetFromCache<List<NotifyMessage>>(email);
+            if (history == null)
+            {
+                return BadRequest(new ResponseDTO(404, "history not found"));
+            }
+            foreach (var notify in history)
+            {
+                notify.IsSeen = true;
+            }
+            await _cacheProvider.SetCache<List<NotifyMessage>>(email, history);
+
+            return Ok(new ResponseDTO(204, "Successfully setSeen"));
         }
     }
 }
--- Services/Cache/ICacheProvider.cs
+++ Services/Cache/ICacheProvider.cs
@@ -7,6 +7,10 @@ namespace BackEnd.Services.Cache
 {
     public interface ICacheProvider
     {
-        
+        Task<T> GetFromCache<T>(string key) where T : class;
+        Task SetCache<T>(string key, T value) where T : class;
+        Task ClearCache(string key);
+        Task AddValueToKey<T>(string key, T value);
+        Task<List<T>> GetAllValueFromCache<T>() where T : class;
     }
 }
